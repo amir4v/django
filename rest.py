0a860dd1cf0183a3c3d60150078385a12eca79e8
Amir <84547283+amir4v@users.noreply.github.com>
2024-01-09 16:42:46 +0330

    Update rest.py

--- rest.py
+++ rest.py
@@ -1,3 +1,4 @@
+#
 # rest_framework.permissions
 class IsNotAuthenticated(BasePermission):
     """
@@ -6,4 +7,23 @@ class IsNotAuthenticated(BasePermission):
 
     def has_permission(self, request, view):
         return not bool(request.user and request.user.is_authenticated)
+#
+# rest_framework.permissions
+# usage: permissions.IsInGroup().dev__admin, # means: for these groups: (dev, admin)
+from . import extra_path as exp
+class IsInGroup(BasePermission):
+    def __getattribute__(self, __name: str) -> Any:
+        class Is(BasePermission):
+            def has_permission(self, request, view):
+                return bool(
+                    request.user and
+                    exp.is_member(request.user, self.group)
+                )
+        
+        group = __name.split('__')
+        
+        _is = Is
+        _is.group = group
+        
+        return _is
 #
